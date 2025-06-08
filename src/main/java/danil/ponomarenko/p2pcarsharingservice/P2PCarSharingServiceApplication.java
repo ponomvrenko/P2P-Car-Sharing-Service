eed0844de8f14970ad08cc3b30e6ad45fca6600e
Danil Ponomarenko <danylo.ponomarenko1@nure.ua>
2025-06-08 13:13:56 +0300

    [CONFIG] Added initial infrastructure

---
+++ src/main/java/danil/ponomarenko/p2pcarsharingservice/P2PCarSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package danil.ponomarenko.p2pcarsharingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2PCarSharingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2PCarSharingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/danil/ponomarenko/p2pcarsharingservice/P2PCarSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package danil.ponomarenko.p2pcarsharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2PCarSharingServiceApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}
+*/
+}
